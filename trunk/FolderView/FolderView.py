ec4b426649fe92e5f5780b11f0f03b0151b4cc7d
sunshine1985115 <sunshine1985115@80c2eb4c-f05b-eaac-2b63-2035ae10867d>
2010-09-23 08:41:56 +0000

    first

---
+++ trunk/FolderView/FolderView.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+import gobject
+import gtk
+import rb
+import logging,logging.handlers
+from FolderViewSource import FolderViewSource
+
+log=logging.getLogger('FolderView')
+
+class FolderView (rb.Plugin):
+    def __init__(self):
+        rb.Plugin.__init__(self)
+
+        log.setLevel(logging.DEBUG)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(logging.Formatter('%(name)s %(levelname)-8s %(module)s::%(funcName)s - %(message)s'))
+        log.addHandler(console_handler)
+        
+    def activate(self, shell):
+        self.shell = shell
+
+        group = rb.rb_source_group_get_by_name ("library")
+        self.db = shell.get_property("db")
+        self.entry_type = self.db.entry_register_type("FolderViewEntryType")
+        self.source = gobject.new (FolderViewSource,
+                                    shell=self.shell,
+                                    name="Folder View",
+                                    entry_type=self.entry_type,
+                                    plugin=self,
+                                    source_group=group
+                                    )
+        self.source.set_property( "icon", self.get_folder_closed_icon()) 
+        shell.append_source(self.source, None)
+        shell.register_entry_type_for_source(self.source, self.entry_type)
+        
+    def deactivate(self, shell):
+        log.info("deactivate")
+        self.db.entry_delete_by_type(self.entry_type)
+        self.db.commit()
+        self.db = None
+        self.entry_type = None
+        self.source.delete_thyself()
+        self.source = None
+
+    def get_folder_closed_icon(self):
+        """ Returns a pixbuf with the current theme closed folder icon """
+
+        icon_theme = gtk.icon_theme_get_default()
+        try:
+            icon = icon_theme.load_icon("gnome-fs-directory", 24, 0)
+            return icon
+        except gobject.GError, exc:
+            #print "Can't load icon", exc
+            try:
+                icon = icon_theme.load_icon("gtk-directory", 24, 0)
+                return icon
+            except:
+                #print "Can't load default icon"
+                return None
---
+++ trunk/FolderView/FolderViewSource.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+import gobject
+import gtk
+import gconf
+import os
+import rb,rhythmdb
+import treefilebrowser
+import logging,logging.handlers
+
+log=logging.getLogger('FolderView')
+
+class FolderViewSource(rb.BrowserSource):
+    #__gproperties__ = {'plugin': (rb.Plugin, 'plugin', 'plugin', gobject.PARAM_WRITABLE|gobject.PARAM_CONSTRUCT_ONLY),}
+    
+    def __init__(self):
+        rb.BrowserSource.__init__(self)
+
+        self.entry_list = []
+        
+        self.g_client = gconf.client_get_default()
+        self.library_location = self.g_client.get_list('/apps/rhythmbox/library_locations', gconf.VALUE_STRING)[0]
+        log.info('FolderView')
+
+        #for i in dir(rb):
+        #    print i
+        #print self.db.entry_type_get_by_name("song")
+
+    def do_impl_activate(self):
+        self.shell  = self.get_property('shell')
+        self.db     = self.shell.get_property('db')
+        self.entry_type=self.get_property('entry-type')
+        rb.BrowserSource.do_impl_activate(self)
+
+    def set_entry(self, uri):
+        try:
+            meta = rb.MetaData()
+            meta.load(uri)
+            metadata_list = [
+                            #rb.METADATA_FIELD_TITLE,
+                            rb.METADATA_FIELD_ARTIST,
+                            rb.METADATA_FIELD_ALBUM,
+                            rb.METADATA_FIELD_GENRE,
+                            rb.METADATA_FIELD_TRACK_NUMBER,
+                            rb.METADATA_FIELD_DISC_NUMBER,
+                            rb.METADATA_FIELD_DATE,
+                            rb.METADATA_FIELD_DURATION,
+                            rb.METADATA_FIELD_BITRATE,
+                            rb.METADATA_FIELD_TRACK_GAIN,
+                            rb.METADATA_FIELD_TRACK_PEAK,
+                            rb.METADATA_FIELD_ALBUM_GAIN,
+                            rb.METADATA_FIELD_ALBUM_PEAK,
+                            rb.METADATA_FIELD_MUSICBRAINZ_TRACKID,
+                            rb.METADATA_FIELD_MUSICBRAINZ_ARTISTID,
+                            rb.METADATA_FIELD_MUSICBRAINZ_ALBUMID,
+                            rb.METADATA_FIELD_MUSICBRAINZ_ALBUMARTISTID,
+                            rb.METADATA_FIELD_ARTIST_SORTNAME,
+                            rb.METADATA_FIELD_ALBUM_SORTNAME
+                            ]
+            prop_list = [
+                        #rhythmdb.PROP_TITLE,
+                        rhythmdb.PROP_ARTIST,
+                        rhythmdb.PROP_ALBUM,
+                        rhythmdb.PROP_GENRE,
+                        rhythmdb.PROP_TRACK_NUMBER,
+                        rhythmdb.PROP_DISC_NUMBER,
+                        rhythmdb.PROP_DATE,
+                        rhythmdb.PROP_DURATION,
+                        rhythmdb.PROP_BITRATE,
+                        rhythmdb.PROP_TRACK_GAIN,
+                        rhythmdb.PROP_TRACK_PEAK,
+                        rhythmdb.PROP_ALBUM_GAIN,
+                        rhythmdb.PROP_ALBUM_PEAK,
+                        rhythmdb.PROP_MUSICBRAINZ_TRACKID,
+                        rhythmdb.PROP_MUSICBRAINZ_ARTISTID,
+                        rhythmdb.PROP_MUSICBRAINZ_ALBUMID,
+                        rhythmdb.PROP_MUSICBRAINZ_ALBUMARTISTID,
+                        rhythmdb.PROP_ARTIST_SORTNAME,
+                        rhythmdb.PROP_ALBUM_SORTNAME
+                        ]
+            #log.info('%s:%s'%(uri,type(meta.get(rb.METADATA_FIELD_DURATION))))
+            if meta.get(rb.METADATA_FIELD_DURATION) == 0:
+                return
+            entry = self.db.entry_new(self.entry_type,uri)
+            if meta.get(rb.METADATA_FIELD_TITLE) == None:
+                self.db.set(entry, rhythmdb.PROP_TITLE, os.path.split(uri)[1])
+            else:
+                self.db.set(entry, rhythmdb.PROP_TITLE, meta.get(rb.METADATA_FIELD_TITLE))
+            for i in range(len(metadata_list)):
+                tmp = meta.get(metadata_list[i])
+                if tmp is not None:
+                    self.db.set(entry, prop_list[i], meta.get(metadata_list[i]))
+        except:
+            pass
+
+    def on_treeview_cursor_changed(self, widget):
+        for row in self.props.query_model:
+            entry = row[0]
+            self.db.entry_delete(entry)
+        
+        model = widget.get_model()
+        path = self.filebrowser.get_selected()
+        for item in os.listdir(path):
+            filename = os.path.join(path, item)
+            if os.path.isfile(filename):
+                self.set_entry(path_to_uri(filename))
+        self.db.commit()
+
+    def do_impl_pack_paned (self, paned):
+        self.__paned_box = gtk.HPaned()
+        self.filebrowser = treefilebrowser.TreeFileBrowser(self.library_location[7:])
+        self.scrolled = self.filebrowser.get_scrolled()
+        self.scrolled.set_size_request(200,-1)
+        self.treeview = self.filebrowser.get_view()
+        self.treeview.connect('cursor-changed', self.on_treeview_cursor_changed)
+        self.pack_start(self.__paned_box)
+        self.__paned_box.add1(self.scrolled)
+        self.__paned_box.add2(paned)
+        
+
+def path_to_uri(path):
+    return 'file://'+path
+
+gobject.type_register(FolderViewSource)
